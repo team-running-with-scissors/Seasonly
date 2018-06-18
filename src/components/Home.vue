0f9aeef8d0f85172c9eaf2cce18004461c61183b
Mark Myers <markalope@gmail.com>
2018-06-18 16:03:58 -0700

    changes to home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="home">
+    <h1 class="page-header">eatSeasonal</h1>
+
+    <p>Our eatSeasonal app helps you to consume food in a more environmentally conscious way by identifying and purchasing foods that are in season! Search for ingredients that are in season, find recipes using those items and automatically add them to your shopping list. You can also save your favorite recipes!</p>
     
   </div>
 </template>
@@ -10,6 +13,11 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+h1 {
+ font-family: 'Lobster Two', cursive;
+ font-size: 64px;
+
+}
 
 </style>
