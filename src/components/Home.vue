68b892eb5ee56038868db05b0f1162b2ba3ae3a7
Mark Myers <markalope@gmail.com>
2018-06-18 16:18:25 -0700

    added search to home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,10 +2,19 @@
   <div id="home">
 
     <h1 class="page-header">eatSeasonal</h1>
-
-    <p>Our eatSeasonal app helps you to consume food in a more environmentally conscious way by identifying and purchasing foods that are in season! Search for ingredients that are in season, find recipes using those items and automatically add them to your shopping list. You can also save your favorite recipes!</p>
+    <nav>
+        <router-link to="/">HOME</router-link>
+        &nbsp;
+        <router-link to="/about">ABOUT</router-link>
+        &nbsp;
+        <router-link to="/search">SEARCH</router-link>
+        &nbsp;
+        <router-link to="/user">USER</router-link>
+      </nav>
     
-
+    <router-view></router-view>
+     <p>Our eatSeasonal app helps you to consume food in a more environmentally conscious way by identifying and purchasing foods that are in season! Search for ingredients that are in season, find recipes using those items and automatically add them to your shopping list. You can also save your favorite recipes!</p>
+ 
   </div>
 </template>
 
