79207e38d44b024458ea27993bc28729805f5bda
Erik Withak <ErikAanestad@gmail.com>
2018-06-18 15:11:28 -0700

    search

---
+++ components/Search.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+  <div id="container-whole">
+    <div id="container-search">
+      <input v-model="searchTerm">
+      <button @click.prevent="handleSearch">Search!</button>
+    </div>
+    <div id="container-main">
+
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      searchTerm: null
+    };
+  },
+  methods: {
+    handleSearch() {
+      alert('hi');
+    }
+  }
+}
+</script>
+
+<style scoped>
+#container-search {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+</style>
