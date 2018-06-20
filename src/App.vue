db0a9bde978abb623a32e4a76b3a8a2995994ca8
Erik Withak <ErikAanestad@gmail.com>
2018-06-20 08:40:40 -0700

    gpo

--- src/App.vue
+++ src/App.vue
@@ -156,7 +156,7 @@ Font-Size: 1.75em;
 .fade-enter-active, .fade-leave-active {
   transition: opacity .5s;
 }
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+.fade-enter, .fade-leave-to {
   opacity: 0;
 }
 
