3bb447a5556436c24fc1974745fff973d88e99a1
Mark Myers <markalope@gmail.com>
2018-06-20 10:08:00 -0700

    adding blockquote to homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="home">
     <p class="content">Our Seasonly app helps you to consume food in a more environmentally conscious way by identifying and purchasing foods that are in season! Search for ingredients that are in season this month, find recipes using those items and automatically add them to your shopping list. You can also save your favorite recipes!</p>
+    <blockquote>Supporting local food production is so much healthier for people. It's better for the local economy, and it's a lot of fun.<br />
+    - Jack Johnson</blockquote>
+
+
     <a class="ghost-button" href="/search">Get Started</a>
 
   </div>
@@ -20,6 +24,15 @@ h1 {
 }
 p {
   color: #fff;
+}
+blockquote {
+color: #fff;
+font-size: 24px;
+font-style: italic;
+margin-left: 10%;
+margin-right: 10%;
+border-top: 1px solid white;
+
 }
 .ghost-button {
   display: inline-block;
