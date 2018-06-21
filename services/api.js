60aa82e35ba6c1ea504f1ee34eec2becdcdc0b0d
Mark Myers <markalope@gmail.com>
2018-06-21 12:03:47 -0700

    changes to api.js

--- services/api.js
+++ services/api.js
@@ -4,7 +4,8 @@
 const apiKey = 'K0ACWNU09ihIYSJfG0UmG4u99d9Oj2Da';
 
 // For Everybody! :->
-const URL = 'http://localhost:3000/api';
+//const URL = 'http://localhost:3000/api';
+const URL = 'https://pure-river-67642.herokuapp.com/api';
 
 export {
   signIn,
