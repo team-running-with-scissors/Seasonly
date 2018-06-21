fcb587d53f05386d925105ace407dd96605556be
Robyn Navarro <rnavarro182@gmail.com>
2018-06-20 18:40:03 -0700

    no dupes in the list

--- src/components/RecipeCard.vue
+++ src/components/RecipeCard.vue
@@ -53,7 +53,8 @@ export default {
     addToMasterFavoriteList: {
       type: Function
     },
-    userid: Number
+    userid: Number,
+    userShoppingList: Array
   },
   data() {
     return {
@@ -70,8 +71,9 @@ export default {
         this.toggleRecipe(true);
       }
       else {
+        let filteredIngredients = this.selectedRecipe.Ingredients.filter(el => this.userShoppingList.every(a => el.Name !== a.item));
         let ingredients = {};
-        ingredients = this.selectedRecipe.Ingredients.reduce((acc, cur, i) => {
+        ingredients = filteredIngredients.reduce((acc, cur, i) => {
           acc[i] = { item : cur.Name, selected : false, user_id : parseInt(this.userid) }; // Replace with localstorage userid
           return acc;
         }, []);
