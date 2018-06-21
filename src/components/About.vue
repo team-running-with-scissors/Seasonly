c451fd0a659125c67e8673271081603137ed9158
rbtprograms <rbt61690@gmail.com>
2018-06-21 11:41:31 -0700

    first pass of profiles done

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,25 +2,21 @@
   <div class="container-main" id="about">
     <h1>Team Running with Scissors</h1>
     <h3>Wow look at those guys!</h3>
-    <div>
-      <div class="image" style="backgroundColor=red">
-      </div>
-      <p>Mark is one cool dude</p>
+    <div id="mark" class="profiles">
+      <img id="mark-pic" class="image" src="../../assets/mark_head.jpg">
+      <p class="text">Mark is an aspiring Javascript developer - getting through Alchemy one class at a time. Trying to turn my old age into a feature versus a bug! Follow me on Twitter @markalope - tweets about tech, music, life and of course walruses.</p>
     </div>
-    <div>
-      <div class="image" style="backgroundColor=red">
-      </div>
-      <p>Robert is one cool dude</p>
+    <div id="robert" class="profiles">
+      <img id="robert-pic" class="image" src="../../assets/robert_head.jpg">
+      <p> Robert Thompson is a software developer from Portland, Oregon. You can often times find him lamenting over the fate of the Portland Trailblazers and his sleep schedule. </p>
     </div>
-    <div>
-      <div class="image" style="backgroundColor=red">
-      </div>
-      <p>Erik is one cool dude</p>
+    <div id="erik" class="profiles">
+      <img id="erik-pic" class="image" src="../../assets/erik.jpg">
+      <p>Erik, Midwest born and raised (Fargo) sauntered around for years and wound up in Portland searching for a successful life in music. He soon realized that he wanted to make music for the sake of art, not money. So he decided to fall back on his first passion, computers/programming, dove in head first, and has dedicated all of this spare time to learning the ropes of Web Development. He plans to continue to push himself in hopes of landing a solid job so he can dedicate his spare time to focus on helping himself and others create art for the sake of art.</p>
     </div>
-    <div>
-      <div class="image" style="backgroundColor=red">
-      </div>
-      <p>Robyn is one cool dude</p>
+    <div id="robyn" class="profiles">
+      <img id="robyn-pic" class="image" src="../../assets/robyn_head.jpg">
+      <p>Robyn is a web developer currently training and honing his skills with Alchemy Code Lab. He enjoys playing video games, eating good food (which he sometimes cooks), listening to his favorite podcasts, and traveling.</p>
     </div>
   </div>
 </template>
@@ -36,7 +32,40 @@ h1, h3, p {
   color: #fff;
 }
 .image{
-  width: 50px;
-  height: 50px
+   height: 150px;
 }
+.profiles{
+  display: block;
+  margin-bottom: 20px;
+}
+
+#mark {
+  float: left;
+}
+#mark-pic {
+  float: left;
+}
+
+#robert {
+  float: right;
+}
+#robert-pic {
+  float: right;
+}
+
+#erik {
+  float: left;
+}
+#erik-pic {
+  float: left;
+}
+
+#robyn {
+  margin-bottom: 55px;
+}
+#robyn-pic {
+  float: right;
+}
+
+
 </style>
